7968e76d10f83810b03edc4c79c0ba463fbc4903
Kalsifer <kalsifer742@gmail.com>
2023-02-16 00:53:36 +0100

    aggiunta la possibilità di aggiungere elementi

--- components/AddMenu.js
+++ components/AddMenu.js
@@ -9,78 +9,95 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { useForm } from "react-hook-form";
 import { Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { createMenu } from '../lib/api';
+import { readCookie } from '../lib/cookie';
+import CustomSnackbar from './CustomSnackBar';
+
+const convertTime = (time) => {
+    const times = time.split(":");
+    return times[0] * 60 + times[1] * 1;
+}
 
 export default function AddMenu(props) {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [openSnackBar, setOpenSnackBar] = React.useState(false);
+    const [severity, setSeverity] = React.useState("warning");
+    const [message, setMessage] = React.useState("Lorem Ipsum");
 
-    const submit = () => {
+    const handleSnackbarClose = () => {    
+        setOpenSnackBar(false);
+    };
 
+    const submit = (data) => {
+        createMenu(readCookie("token"), data.name, [], convertTime(data.start_time), convertTime(data.end_time))
+            .then(async (response) => [await response.json(), response])
+            .then(([data, res]) => {
+                if(res.status == 201) {
+                    handleSnackbarClose
+                } else {
+                    setSeverity("error");
+                    setMessage(
+                        <React.Fragment>
+                            <Typography variant='body1'>Errore API</Typography>
+                            <Typography variant='body2'>{data.msg}</Typography>
+                        </React.Fragment>
+                    );
+                    setOpenSnackBar(true);
+                }
+        })        
     }
 
     return (
         <Dialog open={props.open} onClose={props.handleClose}>
-            <DialogTitle>Subscribe</DialogTitle>
+            <DialogTitle>Aggiungi menu</DialogTitle>
             <DialogContent>
-                <DialogContentText>
-                    To subscribe to this website, please enter your email address here. We
-                    will send updates occasionally.
-                </DialogContentText>
                 <Stack component="form" spacing={2} onSubmit={handleSubmit(submit)}>
                     <TextField
-                        error={errors.email}
+                        error={errors.name}
                         required
                         fullWidth
-                        label="Indirizzo email"
-                        helperText={errors.email && errors.email.message}
-                        {...register("email", {
-                            required: {
-                                value: true,
-                                message: "campo obbligatorio"
-                            },
-                            pattern: {
-                                value: (/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/),
-                                message: "email non valida"
-                            }
-                        })}
-                    />
-                    <TextField
-                        error={errors.password}
-                        required
-                        fullWidth
-                        label="Password"
-                        type="password"
-                        helperText={errors.password && errors.password.message}
-                        {...register("password", {
+                        label="Nome"
+                        helperText={errors.name && errors.name.message}
+                        {...register("name", {
                             required: {
                                 value: true,
                                 message: "campo obbligatorio"
                             },
                             minLength: {
-                                value: 12,
-                                message: "lunghezza minima 12 caratteri"
-                            },
-                            maxLength: {
-                                value: 64,
-                                message: "lunghezza massima 64 caratteri"
-                            },
-                            pattern: {
-                                value: (/^(?=.*[a-zA-Z])(.{12,64})$/),
-                                message: "la password non rispetta i requisiti"
+                                value: 1,
+                                message: "lunghezza minima 1 carattere"
                             }
                         })}
                     />
                     <TextField
+                        error={errors.start_time}
                         required
                         fullWidth
-                        label="Alarm clock"
+                        label="Orario di inizio"
                         type="time"
                         defaultValue="12:00"
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                        inputProps={{
-                        step: 300, // 5 min
-                        }}
+                        helperText={errors.start_time && errors.start_time.message}
+                        {...register("start_time", {
+                            required: {
+                                value: true,
+                                message: "campo obbligatorio"
+                            }
+                        })}
+                    />
+                    <TextField
+                        error={errors.end_time}
+                        required
+                        fullWidth
+                        label="Orario di fine"
+                        type="time"
+                        defaultValue="15:00"
+                        helperText={errors.end_time && errors.end_time.message}
+                        {...register("end_time", {
+                            required: {
+                                value: true,
+                                message: "campo obbligatorio"
+                            }
+                        })}
                     />
                     <Button
                         type="submit"
@@ -89,85 +106,10 @@ export default function AddMenu(props) {
                     >
                         <Typography variant="button" color="white">Invia</Typography>
                     </Button>
+                    <Button fullWidth onClick={props.handleClose}>Cancel</Button>
                 </Stack>
             </DialogContent>
-            <DialogActions>
-                <Button onClick={props.handleClose}>Cancel</Button>
-                <Button onClick={props.handleClose}>Subscribe</Button>
-            </DialogActions>
+            <CustomSnackbar open={openSnackBar} handleClose={handleSnackbarClose} severity={severity} message={message}></CustomSnackbar>
         </Dialog>
     );
 }
-
-/*
-<TextField
-    error={errors.email}
-    required
-    fullWidth
-    label="Indirizzo email"
-    helperText={errors.email && errors.email.message}
-    {...register("email", {
-        required: {
-            value: true,
-            message: "campo obbligatorio"
-        },
-        pattern: {
-            value: (/^([_a-z0-9]+[\._a-z0-9]*)(\+[a-z0-9]+)?@(([a-z0-9-]+\.)*[a-z]{2,4})$/),
-            message: "email non valida"
-        }
-    })}
-/>
-<TextField
-    error={errors.password}
-    required
-    fullWidth
-    label="Password"
-    type="password"
-    helperText={errors.password && errors.password.message}
-    {...register("password", {
-        required: {
-            value: true,
-            message: "campo obbligatorio"
-        },
-        minLength: {
-            value: 12,
-            message: "lunghezza minima 12 caratteri"
-        },
-        maxLength: {
-            value: 64,
-            message: "lunghezza massima 64 caratteri"
-        },
-        pattern: {
-            value: (/^(?=.*[a-zA-Z])(.{12,64})$/),
-            message: "la password non rispetta i requisiti"
-        }
-    })}
-/>
-<TextField
-    id="time"
-    label="Alarm clock"
-    type="time"
-    defaultValue="07:30"
-    InputLabelProps={{
-    shrink: true,
-    }}
-    inputProps={{
-    step: 300, // 5 min
-    }}
-    sx={{ width: 150 }}
-/>
-<Button
-    type="submit"
-    fullWidth
-    variant="contained"
->
-    <Typography variant="button" color="white">Accedi</Typography>
-</Button>
-<Button
-    fullWidth
-    variant="contained"
-    onClick={() => {router.push("/signup")}}
->
-    <Typography variant="button" color="white">Registrati</Typography>
-</Button>
-*/
